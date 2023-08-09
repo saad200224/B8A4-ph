2db5be461a245b311e25de277db2c0734dedf302
saad200224 <saad19021994@outlook.com>
2023-08-09 18:11:25 +0600

    updated

--- problem-03.js
+++ problem-03.js
@@ -4,7 +4,19 @@ function sortMaker(arr) {
         return 'equal'
     }
     else if ( arr[0] > 0 && arr[1] > 0 ) {
-        return 'Array Reverse Korte hobe'
+        const maxElement = arr[0];
+        const reverseArray = [];
+        for (let i = 0; i < arr.length; i++){
+            const index =i;
+            const element = arr[index];
+            if( element > maxElement){
+                maxElement = element;
+                reverseArray.push(element); 
+            }
+            return reverseArray;
+        }
+        return reverseArray;
+        
     }
     else if ( arr[0] < 0 || arr[1] < 0 ){
         return 'Invalid Input'
