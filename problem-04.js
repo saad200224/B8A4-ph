8bf69de39770c3ccf868a8d86f96e1de94202568
saad200224 <saad19021994@outlook.com>
2023-08-10 00:13:02 +0600

    p5

--- problem-04.js
+++ problem-04.js
@@ -1,12 +1,16 @@
 
 function findAddress(address){
 
+    if(typeof address !== "object"){
+        return "Please provide valid object"
+    } else{
+
     const street = address.street
     const house = address.house
     const society = address.society
 
     return street+','+house+','+society
-  
+    }
 }
 const inputObj = {
     street: 10,
--- problem-05.js
+++ problem-05.js
@@ -1,11 +1,18 @@
 function canPay(changeArray, totalDue) {
 
-
-
-
-
+    let sum  = 0;
+    for ( let i = 0; i < moneyInHand.length; i++){
+        sum = sum + moneyInHand[i];
+    }
+    if ( sum >= chipsPrice){
+        return true;
+    }else
+    {
+        return false;
+    }
 }
 
+const moneyInHand = [1, 5, 5];
+const chipsPrice = 10;
 
-
-canPay(changeArray, totalDue)
+console.log(canPay(moneyInHand, chipsPrice))
