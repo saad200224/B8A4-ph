7ad9f3c1dc07d6f0e0c74a424bbfe220a157e718
saad200224 <saad19021994@outlook.com>
2023-08-10 11:39:00 +0600

    all done

--- problem-05.js
+++ problem-05.js
@@ -3,41 +3,41 @@ function canPay(changeArray, totalDue) {
   if (!Array.isArray(changeArray)) {
     return "For First Input: Please provide an array.";
   }
-  
+
   else if (typeof totalDue !== "number" || totalDue < 0) {
     return "For Second Input: Please provide a positive number only.";
   }
 
   else {
+
     if (changeArray.length == 0 || changeArray.length <3) {
       return "Please input 3 positive numbers inside array.";
     }
 
     else {
 
-      for (let i = 0; i < changeArray.length; i++) {
-        if (typeof changeArray[i] !== "number") {
-          console.log(changeArray[i])
-          return "please provide me an array of number";
-        }
+      for (let i = 0; i < changeArray.length; i++){
 
-        else{
-          let sum = 0;
-          for (let i = 0; i < changeArray.length; i++) {
-            sum = sum + changeArray[i];
+          if (typeof changeArray[i] !== "number" || changeArray[i] <0 ) {
+              return "Please provide only positive numbers inside array.";
           }
-          if (sum >= totalDue) {
-            return true;
-          } else {
-            return false;
-          }
-        }
+      }
+
+      let sum = 0;
+
+      for (let i = 0; i < changeArray.length; i++) {
+          sum = sum + changeArray[i];
+      }
+      if (sum >= totalDue) {
+          return true;
+      }else {
+          return false;
       }
     }
   }
 }
 
-const changeArray = [1, 4, 5];
-const totalDue = 10;
+const moneyArray = [1, 4, 5];
+const chipsPrice = 10;
 
-console.log(canPay(changeArray, totalDue));
+console.log(canPay(moneyArray, chipsPrice));
--- submit.js
+++ submit.js
@@ -59,3 +59,41 @@ function findAddress(address){
     }
 }
 
+function canPay(changeArray, totalDue) {
+
+    if (!Array.isArray(changeArray)) {
+      return "For First Input: Please provide an array.";
+    }
+  
+    else if (typeof totalDue !== "number" || totalDue < 0) {
+      return "For Second Input: Please provide a positive number only.";
+    }
+  
+    else {
+  
+      if (changeArray.length == 0 || changeArray.length <3) {
+        return "Please input 3 positive numbers inside array.";
+      }
+  
+      else {
+  
+        for (let i = 0; i < changeArray.length; i++){
+  
+            if (typeof changeArray[i] !== "number" || changeArray[i] <0 ) {
+                return "Please provide only positive numbers inside array.";
+            }
+        }
+  
+        let sum = 0;
+  
+        for (let i = 0; i < changeArray.length; i++) {
+            sum = sum + changeArray[i];
+        }
+        if (sum >= totalDue) {
+            return true;
+        }else {
+            return false;
+        }
+      }
+    }
+  }
